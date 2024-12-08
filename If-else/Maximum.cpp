07191fdd237a561a31f5871f09e685c7e8f81956
mdShehzar <mohammadshehzar@gmail.com>
2024-12-09 02:22:28 +0500

    Updated

---
+++ If-else/Maximum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int n1, n2;
+	
+	cout<<"Enter a number (n1) : ";
+	cin>>n1;
+	cout<<"Enter another number (n2): ";
+	cin>>n2;
+	
+	if (n1>n2){
+		cout<<"n1 is greater."<<endl;
+	}
+	else {
+		cout<<" n2 is greater."<<endl;
+	}
+	
+}
